6601a3444669558bda1d63ba4980aade2cd828c7
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2024-11-24 15:59:59 +0530

    Uploading to GitHub

--- src/main/java/com/arena/Arena.java
+++ src/main/java/com/arena/Arena.java
@@ -9,7 +9,7 @@ public class Arena {
     private Player player2;
     private Dice dice;
 
-    public Arena(Player player1,Player player2){
+    public Arena(Player player1, Player player2){
         this.player1 = player1;
         this.player2 = player2;
         this.dice = new Dice();
